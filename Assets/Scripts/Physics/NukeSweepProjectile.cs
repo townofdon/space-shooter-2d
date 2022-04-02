ad9feaaabf26c64e2bf1da33146c3941b1cf32b1
Don Juan Javier <townofdon@gmail.com>
2022-04-01 20:38:59 -0400

    disable nuke projectile sweep

--- Assets/Scripts/Physics/NukeSweepProjectile.cs
+++ Assets/Scripts/Physics/NukeSweepProjectile.cs
@@ -17,25 +17,25 @@ namespace Physics {
             rb = GetComponentInParent<Rigidbody2D>();
         }
 
-        void FixedUpdate() {
-            if (!isSweeping) return;
-            if (shockwaveOrigin == null) return;
-            shockwaveRadius = (shockwaveOrigin.lossyScale.x / 2f);
-            transform.position = shockwaveOrigin.position + heading * shockwaveRadius + heading * edgeFudge;
-        }
+        // void FixedUpdate() {
+        //     if (!isSweeping) return;
+        //     if (shockwaveOrigin == null) return;
+        //     shockwaveRadius = (shockwaveOrigin.lossyScale.x / 2f);
+        //     transform.position = shockwaveOrigin.position + heading * shockwaveRadius + heading * edgeFudge;
+        // }
 
-        void OnTriggerEnter2D(Collider2D other) {
-            if (isSweeping) return;
-            if (!Utils.IsObjectOnScreen(gameObject)) return;
-            if (other.tag == UTag.NukeShockwave) {
-                shockwaveOrigin = other.transform;
-                if ((transform.position - shockwaveOrigin.position).magnitude < shockwaveOrigin.lossyScale.x / 2f - edgeFudge) return;
-                heading = (transform.position - shockwaveOrigin.position).normalized;
-                isSweeping = true;
-                if (rb != null) rb.isKinematic = true;
-                return;
-            }
-        }
+        // void OnTriggerEnter2D(Collider2D other) {
+        //     if (isSweeping) return;
+        //     if (!Utils.IsObjectOnScreen(gameObject)) return;
+        //     if (other.tag == UTag.NukeShockwave) {
+        //         shockwaveOrigin = other.transform;
+        //         if ((transform.position - shockwaveOrigin.position).magnitude < shockwaveOrigin.lossyScale.x / 2f - edgeFudge) return;
+        //         heading = (transform.position - shockwaveOrigin.position).normalized;
+        //         isSweeping = true;
+        //         if (rb != null) rb.isKinematic = true;
+        //         return;
+        //     }
+        // }
     }
 }
 
