8369550f20f7c547d3d4de7c861059adca8c574c
Don Juan Javier <townofdon@gmail.com>
2022-04-04 20:13:44 -0400

    remove kiwicoder behaviourtree - was causing build to fail

--- Assets/Scripts/BehaviourTree/Actions/Kamikaze.cs
+++ Assets/Scripts/BehaviourTree/Actions/Kamikaze.cs
@@ -1,16 +1,16 @@
-using TheKiwiCoder;
+// using TheKiwiCoder;
 
-public class Kamikaze : ActionNode {
-    protected override void OnStart() {
-        if (context.movement == null) return;
-        context.movement.SetKamikaze(true);
-    }
+// public class Kamikaze : ActionNode {
+//     protected override void OnStart() {
+//         if (context.movement == null) return;
+//         context.movement.SetKamikaze(true);
+//     }
 
-    protected override void OnStop() { }
+//     protected override void OnStop() { }
 
-    protected override State OnUpdate() {
-        if (context.movement == null) return State.Failure;
-        if (context.player == null || !context.player.isAlive) return State.Failure;
-        return State.Running;
-    }
-}
+//     protected override State OnUpdate() {
+//         if (context.movement == null) return State.Failure;
+//         if (context.player == null || !context.player.isAlive) return State.Failure;
+//         return State.Running;
+//     }
+// }
